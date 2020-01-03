3687e7c456074548dc874fedc311adda9be1fd29
csy <476234335@qq.com>
2020-01-03 16:21:39 +0800

    daily modify 2020-1-3

---
+++ backup_auto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# to auto backup some files to the folder ~/csy_git/file_backup/
+
+cd
+
+cp .vimrc ~/csy_git/file_backup/
+
+cp .bashrc ~/csy_git/file_backup/
+
+cp /etc/apt/sources.list ~/csy_git/file_backup/
+
+exit 0
