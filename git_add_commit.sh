ad2675888189e3ff2210592e35b50f4f386e9422
csy <476234335@qq.com>
2020-01-03 09:25:07 +0800

    test commit2

--- git_add_commit.sh
+++ git_add_commit.sh
@@ -4,10 +4,20 @@
 
 read -p "Please input the commit:" COMMIT
 
+echo "________________git status_______________"
+
 git status
+
+echo "________________git add_______________"
 
 git add .
 
+echo "________________git commit_______________"
+
 git commit -m "$COMMIT"
+
+echo "________________git status_______________"
+
+git status
 
 exit
