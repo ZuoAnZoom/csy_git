a046db77024661fac7483d92932a6f760cf1e016
csy <476234335@qq.com>
2020-01-03 09:04:01 +0800

    add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# a bash script for update
+
+echo "————————————————————————————————————————————————————————————————————"
+
+echo "Begin update & upgrade......"
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+exit
